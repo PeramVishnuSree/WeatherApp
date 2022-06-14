42a28d28830813548b5a9d6bb5f9839753e6d0f4
vishnu <vjp5266@psu.edu>
2022-06-14 12:49:41 -0700

    final commit

--- weather/weather/settings.py
+++ weather/weather/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+import django_on_heroku
 import dj_database_url
 
 
@@ -130,4 +130,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
